89a594117576ca513ff9c99bbc96379753b17094
Shivam <Shivamrshukla@gmail.com>
2021-08-29 14:00:54 +0530

    Added optimisation in the file

---
+++ footer.php
@@ -0,0 +1,50 @@
+<footer class="footer ">
+    <hr>
+    <div class="container">
+        <div class="row">
+          <div class="col">
+            <p class="Footer_text">
+              <a href="#">
+                <strong class="Bold_text"> Phone </strong>
+              </a>
+            </p>
+              <p class="light_text">
+                9028705939
+              </p>
+          </div>
+          <div class="col">
+            <p class="Footer_text">
+              <a href="#">
+                <strong class="Bold_text" >Email </strong> 
+              </a>
+            </p>
+            <p class="light_text"> Shivamrshukla0@gmail.com </p>
+          </div>
+          <div class="col">
+            <p class="Footer_text"> 
+              <a href="#">
+               <strong class="Bold_text"> Social Media </strong> 
+              </a> 
+            </p>
+            <p class="Footer_text Social_media">
+              <a href="https://www.facebook.com/campaign/landing.php?&campaign_id=973072061&extra_1=s%7Cc%7C231348943360%7Ce%7Cfacebook%7C&placement=&creative=231348943360&keyword=facebook&partner_id=googlesem&extra_2=campaignid%3D973072061%26adgroupid%3D54006406691%26matchtype%3De%26network%3Dg%26source%3Dnotmobile%26search_or_content%3Ds%26device%3Dc%26devicemodel%3D%26adposition%3D%26target%3D%26targetid%3Dkwd-297690534863%26loc_physical_ms%3D1007785%26loc_interest_ms%3D%26feeditemid%3D%26param1%3D%26param2%3D&gclid=Cj0KCQjwna2FBhDPARIsACAEc_WW_XMrY9rWrngUoDzauz6Ahfd00LnrEAhaMTJaLJCS69ENwukfJ_gaAgzvEALw_wcB"> 
+                <i class="fab fa-facebook"></i>
+              </a>
+              <a href="https://www.instagram.com/_shivam_._s/">
+                 <i class="fab fa-instagram"></i> 
+              </a>
+              <a href="https://www.linkedin.com/in/shivam-s-9969a2202"> 
+                 <i class="fab fa-linkedin"></i> 
+              </a>
+              <a href="https://twitter.com/Shivam17719245">
+                 <i class="fab fa-twitter"></i> 
+              </a>
+            </p>
+          </div>
+          <div class="col ">
+            <p class="light_text copy_right">© 2021 By Shivam Shukla <br>
+            Proudly created by Myself</p>
+          </div>
+        </div>
+    </div>
+  </footer>
---
+++ project.php
@@ -0,0 +1,142 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags --> 
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+    integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/MStyle.css">
+  <link rel="stylesheet" href="css/project.css">
+
+  <!-- font css -->
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Courgette&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/fontawesome.min.css">
+
+  <!-- font awosome -->
+  <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
+    integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+  <title>Project</title>
+</head>
+
+<body>
+    <!-- header_Section -->
+  <?php 
+      include 'header.php';
+  ?>
+  <div class="header">
+    <div class="row">
+      <div class="col-sm-6 left_col">
+        <img src="image/logo.png" alt="logo" align="left">
+      </div>
+      <div class="col Right_col">
+        <p>Project</p>
+      </div>
+    </div>
+    <p id="paragraph">
+      I created this project, from my knowledge which I have learned, there is some good project also that I created with lots of effort. I am constantly learning and creating new things, applying that knowledge </p>
+  </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12">
+        <div class="card flex-md-row mb-4 box-shadow h-md-250 card_size">
+          <div class="card-body d-flex flex-column align-items-start project_text">
+            <h3 class="mb-3 ">
+              <a href="https://github.com/Shivam90-sketch/Bank_Account">My Bank</a>
+            </h3>
+            <p class="card-text mb-auto">This is a My Bank is created using C++ language . In this users can open their account, by proving some basic detail. And there is also a small Atm machine so that users can transact. This is a wonderful My bank Application.</p>
+          </div>
+          <img class="card-img-right flex-auto d-none d-md-block" data-src="holder.js/200x250?theme=thumb"
+            alt="Thumbnail [200x250]" style="width: 200px; height: 250px;"
+            src="image/bank.png"data-holder-rendered="true">
+        </div>
+      </div>
+    </div> 
+    
+    <div class="row">
+      <div class="col-lg-12">
+        <div class="card flex-md-row mb-4 box-shadow h-md-250 card_size">
+          <div class="card-body d-flex flex-column align-items-start project_text">
+            <h3 class="mb-3">
+              <a class="text-dark" href="https://github.com/Shivam90-sketch/Tictactoe">Tick Tac toe</a>
+            </h3>
+            <p class="card-text mb-auto">This game is created in the C language. In this game, the Player has to select only the position to add respectively "X" or "0" and at the end, it show's who wins the game.</p>
+          </div>
+          <img class="card-img-right flex-auto d-none d-md-block" data-src="holder.js/200x250?theme=thumb"
+            alt="Thumbnail [200x250]" style="width: 200px; height: 250px;"
+            src="image/Tic.png"data-holder-rendered="true">
+        </div>
+      </div>
+    </div> 
+
+    <div class="row">
+      <div class="col-lg-12">
+        <div class="card flex-md-row mb-4 box-shadow h-md-250 card_size">
+          <div class="card-body d-flex flex-column align-items-start project_text">
+            <h3 class="mb-3">
+              <a class="text-dark" href="https://github.com/Shivam90-sketch/Atm_machine">Atm Machine</a>
+            </h3>
+            <p class="card-text mb-auto">This atm machine is created using C ++ language. In this machine, you have to enter your amount and start your transaction. It shows if your balance is low and remaining balance and etc.</p>
+          </div>
+          <img class="card-img-right flex-auto d-none d-md-block" data-src="holder.js/200x250?theme=thumb"
+            alt="Thumbnail [200x250]" style="width: 200px; height: 250px;"
+            src="image/Atm.jpg"data-holder-rendered="true">
+        </div>
+      </div>
+    </div>
+    
+    <div class="row">
+      <div class="col-lg-12">
+        <div class="card flex-md-row mb-4 box-shadow h-md-250 card_size">
+          <div class="card-body d-flex flex-column align-items-start project_text">
+            <h3 class="mb-3">
+              <a class="text-dark" href="https://shivam90-sketch.github.io/Game_Website/">Dice game</a>
+            </h3>
+            <p class="card-text mb-auto">This game is created using Html, CSS, and javascript. Here the player has to refresh the page only, when the page refreshes it rolls dice and show's how who wins the game. It happens randomly. So only the player has to refresh the page only.</p>
+          </div>
+          <img class="card-img-right flex-auto d-none d-md-block" data-src="holder.js/200x250?theme=thumb"
+            alt="Thumbnail [200x250]" style="width: 200px; height: 250px;"
+            src="image/Dice.PNG"data-holder-rendered="true">
+        </div>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-lg-12">
+        <div class="card flex-md-row mb-4 box-shadow h-md-250 card_size">
+          <div class="card-body d-flex flex-column align-items-start project_text">
+            <h3 class="mb-3">
+              <a class="text-dark" href="https://shivam90-sketch.github.io/Mobile_shop/">Phone Page</a>
+            </h3>
+            <p class="card-text mb-auto Tex-center">This is a phone page. Here it shows a Phone specification, The battery, Camera and processor, etc. It is part of an e-commerce website page. Using javascript I have added a beautiful animation.</p>
+          </div>
+          <img class="card-img-right flex-auto d-none d-md-block" data-src="holder.js/200x250?theme=thumb"
+            alt="Thumbnail [200x250]" style="width: 200px; height: 250px;"
+            src="image/phone.PNG"data-holder-rendered="true">
+        </div>
+      </div>
+    </div>
+
+  </div>
+
+  
+  <!-- footer_Section -->
+   <?php 
+      include 'footer.php';
+    ?>
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+    integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+    crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+    integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+    crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+    integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+    crossorigin="anonymous"></script>
+</body>
+
+</html>
